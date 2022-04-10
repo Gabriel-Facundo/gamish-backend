701a6c778cb5813f451e81f5b17b0d21f206205c
Gabriel Facundo <gfacundo@outlook.com>
2022-04-09 21:26:18 -0300

    criado endpoint para login, sem lógica criada

---
+++ gamish-backend/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using System.Threading.Tasks;
+
+namespace gamish_backend.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody]Login login)
+        {
+            return Ok();
+        }
+    }
+}
